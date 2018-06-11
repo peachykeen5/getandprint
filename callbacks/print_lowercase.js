4f7a683809fb31759b4156ee7ef43a9fdebe3bd4
peachykeen5 <lexi.keen@gmail.com>
2018-06-11 21:58:04 +0000

    converts string to lowercase

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -2,13 +2,11 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step6/uppercase.html'
+    path: '/http-examples/step6/lowercase.html'
 };
 
 function printUpperCase(html) {
-
-    /* Write your code here! */
-
+    console.log(html.toLowerCase());
 }
 
 getHTML(requestOptions, printUpperCase);
