9f63d98ee7d56809f50726effd3a058d6290ec71
peachykeen5 <lexi.keen@gmail.com>
2018-06-11 21:35:24 +0000

    end stream and lint updated

--- getandprintParams.js
+++ getandprintParams.js
@@ -1,17 +1,19 @@
 var https = require('https');
 var pie = "";
 
-    function getAndPrintHTML(options) {
+function getAndPrintHTML(options) {
 
-        https.get(options, function (stream) {
-            stream.setEncoding('utf8');
-            stream.on('data', function (data) {
-                pie += data;
-                console.log(pie);
-            });
+    https.get(options, function (stream) {
+        stream.setEncoding('utf8');
+        stream.on('data', function (data) {
+            pie += data;
+            console.log(pie);
+        });
+        stream.on('end', function () {
+            console.log("stream has ended")
         })
-
-    }
+    })
+}
 getAndPrintHTML({
     host: 'sytantris.github.io',
     path: '/http-examples/step3.html'
