d22b444fd7f2d62159ff4f289976b95bcf3ad585
peachykeen5 <lexi.keen@gmail.com>
2018-06-11 20:43:46 +0000

    updated to make more modular

--- getandprintParams.js
+++ getandprintParams.js
@@ -1,11 +1,18 @@
-function getAndPrintHTML (options) {
+var https = require('https');
+var pie = "";
 
-    /* Add your code here */
-  
-  }
-  
-  var requestOptions = {
+    function getAndPrintHTML(options) {
+
+        https.get(options, function (stream) {
+            stream.setEncoding('utf8');
+            stream.on('data', function (data) {
+                pie += data;
+                console.log(pie);
+            });
+        })
+
+    }
+getAndPrintHTML({
     host: 'sytantris.github.io',
     path: '/http-examples/step3.html'
-  };
-  
+});
