853b6388f0074105e4df30a9e0ee836e8d7a6c1d
peachykeen5 <lexi.keen@gmail.com>
2018-06-11 19:56:15 +0000

    initial commit

---
+++ getandprintParams.js
@@ -0,0 +1,11 @@
+function getAndPrintHTML (options) {
+
+    /* Add your code here */
+  
+  }
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+  
