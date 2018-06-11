591b6fc9a31ca5331386d5514bd7382f63a6f69d
peachykeen5 <lexi.keen@gmail.com>
2018-06-11 19:54:47 +0000

    updated to print with buffered strings

---
+++ getandprintHTML.js
@@ -0,0 +1,19 @@
+function getAndPrintHTML() {
+
+    var https = require('https');
+    var pie = ("");
+
+    var requestOptions = {
+        host: 'sytantris.github.io',
+        path: '/http-examples/step2.html'
+    };
+    https.get(requestOptions, function (stream) {
+        stream.setEncoding('utf8');
+        stream.on('data', function (data) {
+            pie += data;
+            console.log(pie);
+        });
+    })
+
+}
+getAndPrintHTML()
